806a01d8e2c4c0a2e68f760484fc1b5e63218a7f
Ruhsaku <78037944+Ruhsaku@users.noreply.github.com>
2022-10-17 10:56:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+#Hangman
+# from replit import clear
+import random
+# import hangman_words
+# import hangman_art
+from hangman_art import logo, stages
+from hangman_words import word_list
+
+print(logo)
+end_of_game = False
+chosen_word = random.choice(word_list)
+word_lenght = len(chosen_word)
+lives = 6
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for _ in range(word_lenght):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    # clear()
+    
+    if guess in display:
+      print(f"You've already guessed {guess}")
+    
+    #Check guessed letter
+    for position in range(word_lenght):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter      
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose!")
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+    
+    if "_" not in display:
+        end_of_game = True
+        print("You win!")
+    print(stages[lives])
